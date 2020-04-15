1d67048b98c6b78523104bfc7b61d771e31645fc
Yuval Ron <yuval@musomind.com>
2020-04-15 20:18:03 +0200

    nest-restaurant-api implementing Password.js with local and JWT auth

--- nest-restaurant-api/src/app.module.ts
+++ nest-restaurant-api/src/app.module.ts
@@ -5,9 +5,11 @@ import { ItemsController } from "./items/items.controller";
 import { ItemsService } from './items/items.service';
 import { ShoppingCartController } from "./shopping-cart/shopping-cart.controller";
 //import { AuthenticationMiddleware } from './common/authentication.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, ItemsController, ShoppingCartController],
   providers: [AppService, ItemsService],
 })
