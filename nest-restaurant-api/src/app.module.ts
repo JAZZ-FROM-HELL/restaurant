616b86fac4ac5d545e8658fda6a8ef1d97484ad3
Yuval Ron <yuval@musomind.com>
2020-04-21 16:08:58 +0200

    nest-restaurant-api
    Global logging interceptor

--- nest-restaurant-api/src/app.module.ts
+++ nest-restaurant-api/src/app.module.ts
@@ -1,16 +1,13 @@
-import { Module, } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule, RequestMethod,} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShoppingCartController } from "./shopping-cart/shopping-cart.controller";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
-//import { APP_GUARD } from '@nestjs/core';
-
-//import { AuthenticationMiddleware } from './common/authentication.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-//import { RolesGuard} from "./common/roles.guard";
+
 
 @Module({
   imports: [AuthModule, UsersModule, ItemsModule,
@@ -28,15 +25,4 @@ import { UsersModule } from './users/users.module';
   controllers: [AppController, ShoppingCartController],
   providers: [AppService],
 })
-export class AppModule {
-
-  /*public configure(consumer: MiddlewareConsumer) {
-    consumer
-        .apply(AuthenticationMiddleware)
-        .forRoutes(
-            { path: '/items', method: RequestMethod.POST },
-            { path: '/shopping-cart', method: RequestMethod.POST },
-        );
-  }*/
-
-}
+export class AppModule { }
